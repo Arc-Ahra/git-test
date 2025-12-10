c5cd2b9de132628f9dfeb748a96b85b5dc2a1780
Ahra <archoi@arcsquare.ai>
2025-12-10 13:39:44 +0900

    add project

--- Hello/Hello/Form1.cs
+++ Hello/Hello/Form1.cs
@@ -8,23 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Calc
+namespace Hello
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
